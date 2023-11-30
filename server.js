037b3c63a690c566e23ac5d244e54520c7e3b04a
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-30 04:04:53 -0700

    added more strings in array of db.json

--- server.js
+++ server.js
@@ -22,48 +22,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-//gets all the notes
-app.get('/api/notes', (req, res) => {
-  const notes = getNotes();
-  res.json(notes);
-});
-
-//creates a new note
-app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  newNote.id = generateUniqueId(); 
-  const notes = getNotes();
-  notes.push(newNote);
-  writeNotes(notes);
-  res.json(newNote);
-});
-
-//deletes the notes based on the id
-app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    const notes = getNotes();
-    const updatedNotes = notes.filter((note) => note.id !== noteId);
-    writeNotes(updatedNotes);
-    res.json({ success: true, message: 'Note deleted successfully' });
-  });
-
-// Function to read notes from db.json
-const getNotes = () => {
-  const data = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8');
-  return JSON.parse(data) || [];
-};
-
-// Function to write notes to db.json
-const writeNotes = (notes) => {
-  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes), 'utf8');
-};
-
-// Function to generate a unique ID for each note
-const generateUniqueId = () => {
-
-  return Date.now().toString();
-};
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
